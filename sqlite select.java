0b7bdfc7a11cc21ad2921da61426d7fc405efbe4
Harshal Baraskar <37304526+baraskarharshal@users.noreply.github.com>
2018-06-19 12:29:35 +0530

    Create sqlite select.java

---
+++ sqlite select.java
@@ -0,0 +1,23 @@
+ // Fetch Alarm data from database
+  queryString = "SELECT * FROM alarm_tbl WHERE id = '"+id+"'";
+  c = db.rawQuery(queryString, null);
+  if(c.getCount()==0)
+  {
+      dbInstance.msg(this, "Alarm not found!");
+  }
+  alarmBuffer = new StringBuffer();
+
+  while(c.moveToNext())
+  {
+  // get alarm values
+  if(fromActivity.equals("MainActivity")){
+      time = c.getString(1);
+      ampm = c.getString(2);
+      repeatType = c.getString(3);
+      repeatDays = c.getString(4);
+      note = c.getString(5);
+      ringtone = c.getString(7);
+      Log.d("AlarmInfoActivity", "ringtone while fetching : " +ringtone);
+      vibrate_switch = c.getString(8);
+
+   }
