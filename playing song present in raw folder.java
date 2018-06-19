9815ad2dc7527020c2ebd2630227831bf8661d5f
Harshal Baraskar <37304526+baraskarharshal@users.noreply.github.com>
2018-06-19 12:24:57 +0530

    Create playing song present in raw folder.java

---
+++ playing song present in raw folder.java
@@ -0,0 +1,16 @@
+
+// stop already playing song
+  if(mediaPlayer.isPlaying()){
+      mediaPlayer.stop();
+  }
+
+  // Playing selected song
+  RadioGroup rg = (RadioGroup) findViewById(R.id.ringOptions);
+  //RadioButton rd = (RadioButton)ringtoneDialog.findViewById(rg.getCheckedRadioButtonId());
+  selectedSongName = ((RadioButton) findViewById(rg.getCheckedRadioButtonId())).getText().toString();
+  //rg.clearCheck();
+  //rd.setChecked(true);
+  currentSongId = getResources().getIdentifier(selectedSongName, "raw", getPackageName());
+  mediaPlayer = MediaPlayer.create(getApplicationContext(),currentSongId );
+  mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+  mediaPlayer.start();
